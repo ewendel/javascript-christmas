5907782a3f106ffca2d82ad54f8a0d3ae94b2c3d
Erik Wendel <erik.wendel@bekk.no>
2018-11-28 21:50:37 +0100

    react.christmas -> javascript.christmas

--- config.js
+++ config.js
@@ -2,16 +2,17 @@
 // If you want to make this page your own - change the domain name, text and
 // what not here.
 
-module.exports = {
-  name: 'react.christmas',
-  domain: 'https://react.christmas',
-  ogDescription: 'A nice little Christmas calendar for people who love React',
-  firstYearOfContent: 2017,
+
+export default {
+  name: 'javascript.christmas',
+  domain: 'https://javascript.christmas',
+  ogDescription: 'A nice little Christmas calendar for people who love JavaScript',
+  firstYearOfContent: 2018,
   otherChristmasCalendars: [
-    'javascript.christmas',
+    'react.christmas',
     'security.christmas',
     'elm.christmas',
   ],
   handleLegacyLinks: true,
-  googleAnalyticsTrackingId: 'UA-110431089-1'
+  googleAnalyticsTrackingId: 'UA-110431089-3'
 };
---
+++ content/authors/erik-wendel.md
@@ -0,0 +1,11 @@
+---
+name: Kristofer Selbekk
+twitter: ewndl
+github: ewendel
+oneliner: JavaScript Practice Lead at Bekk
+image: https://pbs.twimg.com/profile_images/726163024533401600/2KuW3F5h_400x400.jpg
+---
+
+I love writing - both code and cool articles. I spend most of my days working
+for a large insurance company, but I also love mentoring and teaching other
+devs.
